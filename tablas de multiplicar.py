775727415103786640b16ad4ad5ab8271dcee807
Didier <sasdier@hotmail.com>
2022-01-24 12:54:21 -0500

    tablas de multiplicar

---
+++ tablas de multiplicar.py
@@ -0,0 +1,7 @@
+'''Escribir un programa que muestre por pantalla la tabla de multiplicar del 1 al 10'''
+
+for i in range (1,11):
+    print()
+    for j in range(1,11):
+        resultado = i*j
+        print(f'{i} * {j} = {resultado}')
